4df2ce45128a212e8212048cba8c68c59dd41b98
Ivanenko Andrii <ivanenko0.andrii@gmail.com>
2020-09-16 15:28:42 +0300

    4

--- c1/Program.cs
+++ c1/Program.cs
@@ -27,14 +27,20 @@ namespace c1
             */
 
 
-            Console.Write("Введите свое имя:");
+            Console.Write("Введите имя:");
             string name = Console.ReadLine();
-            Console.WriteLine($"Привет, {name}!");
+
+            Console.Write("Введите возраст:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите рост:");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите размер зарплаты:");
+            decimal salary = Convert.ToDecimal(Console.ReadLine());
 
 
-
-
-
+            Console.WriteLine($"Имя: {name} Возраст: {age} Рост: {height}м Зарплата: {salary}$");
 
             Console.ReadKey();
 
