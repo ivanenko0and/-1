b745f07519abc5bf6d35530e3a6daf8b888ef6ca
Ivanenko Andrii <ivanenko0.andrii@gmail.com>
2020-09-16 15:17:46 +0300

    3

--- c1/Program.cs
+++ c1/Program.cs
@@ -7,17 +7,36 @@ namespace c1
         static void Main(string[] args)
         {
 
+            /*
             String name = "Tom";
             int age = 33;
             bool isEmployed = false;
             double weight = 78.65;
-
+            
             Console.WriteLine($"Имя: {name}");
             Console.WriteLine($"Возраст: {age}");
             Console.WriteLine($"Вес: {weight}");
             Console.WriteLine($"Работает: {isEmployed}");
+            
+
+            Console.WriteLine($"Имя: {name} Возраст: {age} Вес: {weight}м");
+            Console.WriteLine("Имя: {0} Возраст: {2} Вес: {1}м", name, weight, age);
 
 
+
+            */
+
+
+            Console.Write("Введите свое имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}!");
+
+
+
+
+
+
+            Console.ReadKey();
 
         }
     }
